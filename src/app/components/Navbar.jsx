6a4b43278e81512cac4ee1d153a6d96f420f3019
FacundoGerv <facugervasi9@gmail.com>
2024-01-11 12:26:53 -0300

    Updated accound handling

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,91 +3,26 @@ import Link from "next/link";
 import { useRouter, useSearchParams, usePathname } from 'next/navigation';
 import { useEffect, useState } from "react";
 import { UserAuth } from "../context/AuthContext";
-import firebaseApp from "../firebase";
+import firebaseApp from '@/app/firebase';
 import styles from '@/app/styles/navbar.module.css'
 
-//Creación de la funcion default.
+
 const Navbar = () => {
-  const logindwn = document.getElementById('navlist');
+
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const [mode, setMode] = useState(searchParams.get('mode') || 'normal');
-  //Como primera medida llamamos los parametros "user", "googleSignIn" y "logOut" desde nuestro "AuthContext".
   const { user, googleSignIn, logOut } = UserAuth();
-
-  // "cData": Instanciamos una constante con el uso de "useState".
-  // "setCData": Esta función de useState cambia el contenido de "cData".
-  const [cData, setCData] = useState([]);
-
-  // "router": es la constante donde inicializas "useRouter", que este es una función de next.js que nos permite
-  // navegar en el directorio de app.
   const router = useRouter();
 
-  // "db": Se crea una constante con una referencia de la bd.
-  const db = getFirestore(firebaseApp);
-
-  //Esto es una función almacenada, lo que significa que yo la puedo llamar dentro de la función padre.
-  //Es una función asincrona que simplemente llama a lo que seria "googleSignIn" con un await al principio
-  // dado el origen de la misma que tambien es asincronica, lo cual permite que esta se complete antes de continuar. 
   const handleSignIn = async () => {
     try {
       await googleSignIn();
-      router.push(`/?mode=${mode}`); //Usamos la instancia de router que hicimos anteriormente llamando a la función replace, para redirigir.
+      router.push(`/`);
     } catch (error) {
       console.log(error);
     }
   };
-  const handleMode = () => {
-    const newMode = mode === 'normal' ? 'navideño' : 'normal';
-    setMode(newMode);
 
-
-    // Update the URL with the new mode (optional)
-    router.push(`?mode=${newMode}`, undefined, { shallow: true });
-  };
-
-  //Aqui llamamos a todo los datos almacenados en la colección de la bd, llamada genCollection.
-  const fetchCollection = async () => {
-    const Collection = collection(db, 'genCollection');
-    const Snapshot = await getDocs(Collection);
-    const Data = Snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    setCData(Data);
-  };
-
-  //Función que verifica si ya hay una cuenta existente.
-  const handleFirstLogin = async () => {
-    if (user) {
-      const foundItem = cData.find((item) => item.uid === user.uid);
-      if (foundItem) {
-        console.log('User UID found:', foundItem.uid);
-      } else {
-        console.log('User UID not found in database');
-        handleCreateAccount();
-      }
-    }
-  };
-
-  //Función para crear nuevos usuarios, en el caso que no este registrado en la bd.
-  const handleCreateAccount = async () => {
-
-    try {
-
-      //Creamos una referencia donde instanciamos una función llamada "addDoc", que sube los datos 
-      // name y uid a la bd. Estos datos estan llamados desde el autentificador.
-      const docRef = await addDoc(collection(db, 'genCollection'), {
-        name: user.displayName,
-        uid: user.uid,
-        votes: [],
-      });
-      console.log('Document written with ID: ', docRef.id);
-      router.push(`/profile?mode=${mode}`);
-    } catch (error) {
-      console.error('Error adding document: ', error);
-    }
-  }
-
-  //Función para borrar el inicio de sesion.
-  //Llamando a la función "logOut".
   const handleSignOut = async () => {
     try {
       await logOut();
@@ -97,32 +32,29 @@ const Navbar = () => {
     }
   };
 
-  // Update the mode state when the query parameter changes
-  useEffect(() => {
-    setMode(searchParams.get('mode') || 'normal');
-  }, [searchParams]);
-  //Llama a "fetchCollection" cada vez que cambie el estado de "user" y cada vez que renderiza la pag por primera vez.
-  useEffect(() => {
-    fetchCollection();
-  }, [user]);
-
-  //Y este "useEffect" llama a "handleFirstLogin" para vereficar el usuario en la bd cada vez que cambia el estado de "cData".
-  useEffect(() => {
-    handleFirstLogin();
-  }, [cData])
+  var logindwn = document.getElementById('navlist');
 
   const handleNavDropdown = () => {
-    logindwn.style.display === 'block' ? logindwn.style.display = 'none' : logindwn.style.display = 'block';
+    try {
+      logindwn = document.getElementById('navlist');
+      logindwn.style.display === 'block' ? logindwn.style.display = 'none' : logindwn.style.display = 'block';
+    }
+    catch (e) {
+      logindwn = document.getElementById('navlist');
+      console.log(e);
+    }
   }
   const hideNav = () => {
-    logindwn.style.display = 'none';
-  }
-  const test = () => {
-    console.log(pathname);
+    try {
+      logindwn = document.getElementById('navlist');
+      logindwn.style.display = 'none';
+    }
+    catch (e) {
+      logindwn = document.getElementById('navlist');
+      console.error(e.error);
+    }
   }
 
-  //Dentro de "return" se va mostrar la parte visual.
-  //Y en ciertos lugares tiene condicionales como {condicion && ()} que lo que permite esto es que lo que esta () se muestre solo si se cumple la condición.
   return (
     <div className='fixed w-full z-50'>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
@@ -131,31 +63,43 @@ const Navbar = () => {
       </div>
       <div className={styles.navWrapper} id="navlist">
         <ul className={styles.navList}>
-          <Link href={`/?mode=${mode}`} onClick={hideNav}>
+          <Link href={`/`} onClick={hideNav}>
             <li className={`${pathname === '/' ? 'bg-violet-800 bg-opacity-60' : ''} ${styles.navItem}`}>
               <span>Home</span>
             </li>
           </Link>
-          <Link href={`/stock?mode=${mode}`} onClick={hideNav}>
-          <li className={`${pathname === '/stock' ? 'bg-violet-800 bg-opacity-60' : ''} ${styles.navItem}`}>
+          <Link href={`/stock`} onClick={hideNav}>
+            <li className={`${pathname === '/stock' ? 'bg-violet-800 bg-opacity-60' : ''} ${styles.navItem}`}>
               <span>Productos</span>
             </li>
           </Link>
           {user && (
-            <Link href={`/favorites?mode=${mode}`} onClick={hideNav}>
+            <Link href={`/favorites`} onClick={hideNav}>
               <li className={`${pathname === '/favorites' ? 'bg-violet-800 bg-opacity-60' : ''} ${styles.navItem}`}>
                 <span>Favoritos</span>
               </li>
             </Link>
           )}
           <ul className="absolute bottom-0 w-full">
-            <li className={styles.navItem} onClick={test}>
-              <span>Test</span>
-            </li>
-            <li className={styles.navItem} onClick={test}>
-              <span>Test</span>
-            </li>
-            <li className={`${styles.navItem} mb-0`} onClick={test}>
+            {user ? (
+              <>
+                <li className={`${styles.navItem} bg-red-400 bg-opacity-20`} onClick={handleSignOut}>
+                  <span>
+                    Cerrar Sesión
+                  </span>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className={`${styles.navItem} bg-violet-300 bg-opacity-20`} onClick={handleSignIn}>
+                  <span>Iniciar Sesión</span>
+                </li>
+              </>
+            )}
+            <li className={`${styles.navItem} bg-violet-300 bg-opacity-20`} onClick={()=>{
+              console.log(user)
+              .catch((e) => {console.error(e)})
+            }}>
               <span>Test</span>
             </li>
           </ul>
--- src/app/context/AuthContext.js
+++ src/app/context/AuthContext.js
@@ -1,19 +1,27 @@
 import { useContext, createContext, useState, useEffect } from "react";
 import { signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
-import { auth } from "../firebase";
+import { auth } from "@/app/firebase"; 
+import firebaseApp from '@/app/firebase';
+import { updateDoc, setDoc, doc, getFirestore, collection } from 'firebase/firestore'
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const db = getFirestore(firebaseApp)
 
   const googleSignIn = async () => {
     const provider = new GoogleAuthProvider();
     try {
-      await signInWithPopup(auth, provider);
+      const userCredential = await signInWithPopup(auth, provider);
+      const user = userCredential.user;
+
+      // Llama a la función para registrar el usuario en Firestore
+      registerUserInFirestore(user);
+
     } catch (error) {
-      // console.error("Error signing in with Google:", error.message);
-      alert("No posee cuenta autorizada")
+      console.error("Error signing in with Google:", error.message);
+      alert("No posee cuenta autorizada");
     }
   };
 
@@ -22,11 +30,23 @@ export const AuthContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged( auth, (currentUser) => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
     });
     return () => unsubscribe();
-  }, []); // Quité la dependencia 'user' para evitar bucles innecesarios
+  }, []);
+
+  const registerUserInFirestore = (user) => {
+    setDoc(doc(db, 'users', user.uid), {   
+      displayName: user.displayName,
+      email: user.email,
+      isAdmin: false,
+    }).then(() => {
+      console.log("Usuario registrado en Firestore:", user.uid);
+    }).catch((error) => {
+      console.error("Error al registrar el usuario en Firestore:", error.message);
+    });
+  };
 
   return (
     <AuthContext.Provider value={{ user, googleSignIn, logOut }}>
